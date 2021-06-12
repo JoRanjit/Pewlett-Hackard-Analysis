bc11c1c7cdc6fc6742d3521cb0fe03de8a5de2e7
JoRanjit <83322489+JoRanjit@users.noreply.github.com>
2021-06-12 16:51:09 -0700

    Retirement Info

---
+++ Queries/Queries.sql
@@ -0,0 +1,24 @@
+SELECT * FROM departments;
+
+SELECT * FROM employees;
+
+SELECT * FROM salaries;
+
+-- Retirement eligibility
+SELECT first_name,last_name
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1955-12-31'
+AND hire_date BETWEEN '1985-01-01' AND '1988-12-31';
+			 
+SELECT COUNT(*)
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1955-12-31'
+AND hire_date BETWEEN '1985-01-01' AND '1988-12-31';
+
+SELECT first_name,last_name
+INTO retirement_info
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1955-12-31'
+AND hire_date BETWEEN '1985-01-01' AND '1988-12-31';
+
+SELECT * FROM retirement_info;
